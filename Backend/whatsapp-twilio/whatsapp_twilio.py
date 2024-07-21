d6d572c7bd3833014304e750b350ad253f767471
Yash Pande <80116786+yash797@users.noreply.github.com>
2024-07-21 16:33:11 +0530

    Update whatsapp_twilio.py

--- Backend/whatsapp-twilio/whatsapp_twilio.py
+++ Backend/whatsapp-twilio/whatsapp_twilio.py
@@ -1,7 +1,6 @@
 from twilio.rest import Client
 
-account_sid = 'ACf7c673f098935fbc462bf374eb171008'
-auth_token = 'c2ca8e6258b2c24ca679bfb3707a04f9'
+
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
